99bd9bb9853b0a924e85f55f411575d66bf57070
takaion <3522531+takaion@users.noreply.github.com>
2026-06-16 22:43:35 +0900

    refactor: Locale.tsをdata/localesからcomponentsに移動

--- src/components/Locale.ts
+++ src/components/Locale.ts
@@ -1,30 +1,30 @@
-import { Weather } from "../Weather.js";
-import { ZoneId } from "../ZoneWeather.js";
-import ja from "./ja.js";
-import en from "./en.js";
-import { BigFishId } from "../BigFishData.js";
-import { OceanFishingTime } from "../OceanFishingTime.js";
-import { OceanFishingZone } from "../OceanFishingZone.js";
+import { Weather } from "../data/Weather.js";
+import { ZoneId } from "../data/ZoneWeather.js";
+import { BigFishId } from "../data/BigFishData.js";
+import { OceanFishingTime } from "../data/OceanFishingTime.js";
+import { OceanFishingZone } from "../data/OceanFishingZone.js";
+import ja from "../data/locales/ja.js";
+import en from "../data/locales/en.js";
 
 type Locale = "ja" | "en";
 
 function getLocaleData(locale: Locale = "ja") {
     switch (locale) {
-        case "ja": return ja
-        case "en": return en
+        case "ja": return ja;
+        case "en": return en;
     }
 }
 
 export function getWeatherName(weather: Weather, locale?: Locale) {
-    return getLocaleData(locale).weather[weather]
+    return getLocaleData(locale).weather[weather];
 }
 
 export function getZoneName(zone: ZoneId, locale?: Locale) {
-    return getLocaleData(locale).zone[zone]
+    return getLocaleData(locale).zone[zone];
 }
 
 export function getBigFishName(bigFishId: BigFishId, locale?: Locale) {
-    return getLocaleData(locale).bigFish[bigFishId]
+    return getLocaleData(locale).bigFish[bigFishId];
 }
 
 export function getOceanFishingTimeName(time: OceanFishingTime, locale?: Locale) {
