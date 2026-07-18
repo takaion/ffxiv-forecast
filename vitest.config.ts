d02e2b5dadd97e569bdb2648882aa0feed1e6e6d
takaion <3522531+takaion@users.noreply.github.com>
2026-07-19 01:11:16 +0900

    test: vitest対象を `*.test.*` に限定

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     test: {
         globals: true, // This enables global APIs like 'test', 'describe', 'expect'
         // Other Vitest configurations
+        include: ["src/**/*.test.?(c|m)[jt]s?(x)"]
     },
     // Other Vite configurations if applicable
 });
